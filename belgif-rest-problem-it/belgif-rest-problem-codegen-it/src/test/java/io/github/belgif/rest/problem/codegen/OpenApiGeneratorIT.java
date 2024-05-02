e0e910395e74d01521601f2dd982cb51c710f19a
Jimmy Praet <jimmy.praet@ksz-bcss.fgov.be>
2024-05-02 12:52:09 +0200

    Assert generated API and model classes

--- belgif-rest-problem-it/belgif-rest-problem-codegen-it/src/test/java/io/github/belgif/rest/problem/codegen/OpenApiGeneratorIT.java
+++ belgif-rest-problem-it/belgif-rest-problem-codegen-it/src/test/java/io/github/belgif/rest/problem/codegen/OpenApiGeneratorIT.java
@@ -2,8 +2,13 @@ package io.github.belgif.rest.problem.codegen;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import io.github.belgif.rest.problem.codegen.openapi.api.LegacyApi;
+import io.github.belgif.rest.problem.codegen.openapi.api.ProblemApi;
+import io.github.belgif.rest.problem.codegen.openapi.model.SuccessResponse;
 
 class OpenApiGeneratorIT {
 
@@ -14,6 +19,13 @@ class OpenApiGeneratorIT {
     void noProblemModelClassesAreGenerated(String model) {
         assertThatExceptionOfType(ClassNotFoundException.class).isThrownBy(
                 () -> Class.forName("io.github.belgif.rest.problem.codegen.openapi.model." + model));
+    }
+
+    @Test
+    void apiAndModelClassesAreGenerated() {
+        assertThat(ProblemApi.class).hasMethods("getProblem");
+        assertThat(LegacyApi.class).hasMethods("getLegacyProblem");
+        assertThat(SuccessResponse.class).hasMethods("getMessage");
     }
 
 }
