c9788b47c5cfd5d5b8faf57e193a91412f92bfb3
Jimmy Praet <jimmy.praet@ksz-bcss.fgov.be>
2024-11-07 12:42:00 +0100

    Add @ConditionalOnClass(Configuration.class)

--- belgif-rest-problem-spring-boot-3/src/main/java/io/github/belgif/rest/problem/spring/ProblemValidationConfigurationCustomizer.java
+++ belgif-rest-problem-spring-boot-3/src/main/java/io/github/belgif/rest/problem/spring/ProblemValidationConfigurationCustomizer.java
@@ -2,6 +2,7 @@ package io.github.belgif.rest.problem.spring;
 
 import jakarta.validation.Configuration;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.validation.ValidationConfigurationCustomizer;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
  * ValidationConfigurationCustomizer that registers the AnnotationParameterNameProvider.
  */
 @Component
+@ConditionalOnClass(Configuration.class)
 public class ProblemValidationConfigurationCustomizer implements ValidationConfigurationCustomizer {
 
     @Override
