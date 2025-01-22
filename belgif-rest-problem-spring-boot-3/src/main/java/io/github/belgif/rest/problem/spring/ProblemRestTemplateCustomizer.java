2fbea3dee2519f45cb99d01a530016abf076be64
Peter Van den Bosch <peter.vandenbosch@smals.be>
2025-01-22 12:59:41 +0100

    Fix NoSuchMethodError in ProblemResponseErrorHandler (#165)
    Same compatibility error as #98, but this time for
    ProblemResponseErrorHandler (used for RestTemplate and RestClient)
    instead of WebClient.
    
    Moves ProblemResponseErrorHandler and ProblemRestTemplateCustomizer to
    version-specific spring-boot submodules.
    
    I was able to test this fix in another project successfully
    
    ---------
    
    Co-authored-by: Jimmy Praet <jimmy.praet@ksz-bcss.fgov.be>

---
+++ belgif-rest-problem-spring-boot-3/src/main/java/io/github/belgif/rest/problem/spring/ProblemRestTemplateCustomizer.java
@@ -0,0 +1,26 @@
+package io.github.belgif.rest.problem.spring;
+
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * RestTemplateCustomizer that registers the {@link ProblemResponseErrorHandler}.
+ *
+ * @see ProblemResponseErrorHandler
+ */
+@Component
+public class ProblemRestTemplateCustomizer implements RestTemplateCustomizer {
+
+    private final ProblemResponseErrorHandler problemResponseErrorHandler;
+
+    public ProblemRestTemplateCustomizer(ProblemResponseErrorHandler problemResponseErrorHandler) {
+        this.problemResponseErrorHandler = problemResponseErrorHandler;
+    }
+
+    @Override
+    public void customize(RestTemplate restTemplate) {
+        restTemplate.setErrorHandler(problemResponseErrorHandler);
+    }
+
+}
