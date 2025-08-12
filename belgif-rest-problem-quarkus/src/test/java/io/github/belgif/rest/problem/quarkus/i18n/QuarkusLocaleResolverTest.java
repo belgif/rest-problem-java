263ddb5df0c35f3855ba9f4ebe2993725b171725
Jimmy Praet <jimmy.praet@ksz-bcss.fgov.be>
2025-08-12 15:27:52 +0200

    Use QuarkusTest

--- belgif-rest-problem-quarkus/src/test/java/io/github/belgif/rest/problem/quarkus/i18n/QuarkusLocaleResolverTest.java
+++ belgif-rest-problem-quarkus/src/test/java/io/github/belgif/rest/problem/quarkus/i18n/QuarkusLocaleResolverTest.java
@@ -5,64 +5,38 @@ import static org.mockito.Mockito.*;
 
 import java.util.Locale;
 
-import jakarta.enterprise.inject.Instance;
-import jakarta.enterprise.inject.spi.CDI;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import io.github.belgif.rest.problem.i18n.I18N;
 import io.quarkus.arc.Arc;
-import io.quarkus.arc.ArcContainer;
-import io.quarkus.arc.ManagedContext;
+import io.quarkus.test.InjectMock;
+import io.quarkus.test.junit.QuarkusTest;
 
-@ExtendWith(MockitoExtension.class)
+@QuarkusTest
 class QuarkusLocaleResolverTest {
 
     private final QuarkusLocaleResolver localeResolver = new QuarkusLocaleResolver();
 
-    @Mock
-    private CDI cdi;
-
-    @Mock
-    private Instance instance;
-
-    @Mock
+    @InjectMock
     private LocaleHolder localeHolder;
 
-    @Mock
-    private ArcContainer arcContainer;
-
-    @Mock
-    private ManagedContext context;
+    @BeforeEach
+    void activateRequestContext() {
+        // By default active during tests, explicitly activate it to ignore test execution order
+        Arc.container().requestContext().activate();
+    }
 
     @Test
     void getLocale() {
-        try (MockedStatic<CDI> cdiStatic = Mockito.mockStatic(CDI.class);
-                MockedStatic<Arc> arcStatic = Mockito.mockStatic(Arc.class)) {
-            arcStatic.when(Arc::container).thenReturn(arcContainer);
-            when(arcContainer.requestContext()).thenReturn(context);
-            when(context.isActive()).thenReturn(true);
-            cdiStatic.when(CDI::current).thenReturn(this.cdi);
-            when(this.cdi.select(LocaleHolder.class)).thenReturn(instance);
-            when(instance.get()).thenReturn(localeHolder);
-            when(localeHolder.getLocale()).thenReturn(Locale.forLanguageTag("nl-BE"));
-            assertThat(localeResolver.getLocale()).isEqualTo(Locale.forLanguageTag("nl-BE"));
-        }
+        when(localeHolder.getLocale()).thenReturn(Locale.forLanguageTag("nl-BE"));
+        assertThat(localeResolver.getLocale()).isEqualTo(Locale.forLanguageTag("nl-BE"));
     }
 
     @Test
     void getLocaleRequestScopeNotActive() {
-        try (MockedStatic<Arc> arcStatic = Mockito.mockStatic(Arc.class)) {
-            arcStatic.when(Arc::container).thenReturn(arcContainer);
-            when(arcContainer.requestContext()).thenReturn(context);
-            when(context.isActive()).thenReturn(false);
-            assertThat(localeResolver.getLocale()).isEqualTo(I18N.DEFAULT_LOCALE);
-        }
+        Arc.container().requestContext().deactivate();
+        assertThat(localeResolver.getLocale()).isEqualTo(I18N.DEFAULT_LOCALE);
     }
 
 }
