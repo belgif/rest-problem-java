040ef5ecab1c5080874d13336d109c1c70049fb7
pvdb <peter.vandenbosch@smals.be>
2025-10-22 13:29:40 +0200

    change type of problem status to Integer

--- belgif-rest-problem/src/main/java/io/github/belgif/rest/problem/DefaultProblem.java
+++ belgif-rest-problem/src/main/java/io/github/belgif/rest/problem/DefaultProblem.java
@@ -21,9 +21,10 @@ public class DefaultProblem extends Problem implements FluentProblem<DefaultProb
 
     private static final long serialVersionUID = 1L;
 
+    //TODO: status changed to Integer to enable Jackson 3. Do we want to keep this? https://github.com/belgif/rest-problem-java/issues/246
     @JsonCreator
     public DefaultProblem(@JsonProperty("type") URI type, @JsonProperty("href") URI href,
-            @JsonProperty("title") String title, @JsonProperty("status") int status) {
+            @JsonProperty("title") String title, @JsonProperty(value = "status") Integer status) {
         super(type, href, title, status);
     }
 
