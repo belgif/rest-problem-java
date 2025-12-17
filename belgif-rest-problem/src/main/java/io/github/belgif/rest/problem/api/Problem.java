c422ba4d904cbe033f705a31b6a7f92bd41088af
Marcel van Hassel <marcel.vanhassel@smals.be>
2025-12-17 12:54:11 +0100

    Replace reflection for a static array

--- belgif-rest-problem/src/main/java/io/github/belgif/rest/problem/api/Problem.java
+++ belgif-rest-problem/src/main/java/io/github/belgif/rest/problem/api/Problem.java
@@ -1,15 +1,10 @@
 package io.github.belgif.rest.problem.api;
 
-import java.lang.reflect.Field;
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
@@ -48,6 +43,7 @@ import io.github.belgif.rest.problem.DefaultProblem;
 public abstract class Problem extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
+    private static final String[] READ_ONLY_PROPERTIES = { "type", "status", "title" };
 
     private final URI type;
     private URI href;
@@ -121,7 +117,7 @@ public abstract class Problem extends RuntimeException {
 
     @JsonAnySetter
     public void setAdditionalProperty(String name, Object value) {
-        if (!classContains(name)) {
+        if (Arrays.stream(READ_ONLY_PROPERTIES).noneMatch(s -> s.equalsIgnoreCase(name))) {
             additionalProperties.put(name, value);
         }
     }
@@ -159,18 +155,4 @@ public abstract class Problem extends RuntimeException {
     public int hashCode() {
         return Objects.hash(type, href, title, status, detail, instance, additionalProperties);
     }
-
-    private boolean classContains(String propertyName) {
-        return getAllFields(this.getClass()).contains(propertyName.toLowerCase());
-    }
-
-    private static Set<String> getAllFields(Class<?> type) {
-        List<Field> fields = new ArrayList<>();
-        while (type != null && type != Object.class) {
-            fields.addAll(Arrays.asList(type.getDeclaredFields()));
-            type = type.getSuperclass();
-        }
-        return fields.stream().map(f -> f.getName().toLowerCase()).collect(Collectors.toSet());
-    }
-
 }
