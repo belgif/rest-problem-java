d39c58224c20031b86cb4b7ec783fb7a213d4e4c
Jimmy Praet <jimmy.praet@ksz-bcss.fgov.be>
2026-01-08 10:44:01 +0100

    Fix conditional loading of optional components (#280)
    Fixes #279.
    
    * Improve @ConditionalOnClass annotations for Spring Boot 4
    * For consistency, apply the same changes to Spring Boot 3 (even though
    it didn't seem to cause any real issue there)
    * Remove unnecessary jackson 2 dependency

--- belgif-rest-problem-spring-boot-4/src/main/java/io/github/belgif/rest/problem/spring/ProblemRestTemplateCustomizer.java
+++ belgif-rest-problem-spring-boot-4/src/main/java/io/github/belgif/rest/problem/spring/ProblemRestTemplateCustomizer.java
@@ -1,7 +1,9 @@
 package io.github.belgif.rest.problem.spring;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.restclient.RestTemplateCustomizer;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * RestTemplateCustomizer that registers the {@link ProblemResponseErrorHandler}.
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Component;
  * @see ProblemResponseErrorHandler
  */
 @Component
+@ConditionalOnClass({ RestTemplate.class, RestTemplateCustomizer.class })
 public class ProblemRestTemplateCustomizer extends AbstractProblemRestTemplateCustomizer
         implements RestTemplateCustomizer {
 
